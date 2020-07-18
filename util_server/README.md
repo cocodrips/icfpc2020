07437ea0afff1ce770d483af8e0a7a4aa8fbe166
cocodrips <cocodrips@gmail.com>
2020-07-18 15:35:06 +0900

    Add demodulate api

---
+++ util_server/README.md
@@ -0,0 +1,14 @@
+## API
+
+#### /demodulator?value=\<bin>
+
+Request
+    
+    GET: http://host/demodulator?value=11110110000110100110110000
+    
+- param
+   - value: 11110110000110100110110000 みたいなやつ
+    
+Response:
+
+    ( cons ( cons 1 -6 ) ( cons nil nil ) )    
---
+++ util_server/app.py
@@ -0,0 +1,52 @@
+import os
+
+from flask import Flask, request, render_template, Response
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route('/demodulator')
+def demodulate():
+    value = request.args.get("value")
+    args = "bin/demodulator"
+    proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    stdout_value, stderr_value = proc.communicate(value)
+    print(stdout_value)
+    return stdout_value
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def get_resource(path):  # pragma: no cover
+
+    def root_dir():  # pragma: no cover
+        return os.path.abspath(os.path.dirname(__file__))
+
+    def get_file(filename):  # pragma: no cover
+        try:
+            src = os.path.join(root_dir(), filename)
+            return open(src).read()
+        except IOError as exc:
+            return str(exc)
+
+    mimetypes = {
+        ".css": "text/css",
+        ".html": "text/html",
+        ".js": "application/javascript",
+        ".md": "text/plain"
+    }
+    complete_path = os.path.join(root_dir(), path)
+    ext = os.path.splitext(path)[1]
+    mimetype = mimetypes.get(ext, "text/html")
+    content = get_file(complete_path)
+    return Response(content, mimetype=mimetype)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
