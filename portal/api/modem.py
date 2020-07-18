658d516c9394b6c3ae6a3715223ffc5b73fa5267
cocodrips <cocodrips@gmail.com>
2020-07-18 21:57:21 +0900

    Add modulator

--- portal/api/modem.py
+++ portal/api/modem.py
@@ -8,3 +8,11 @@ def demodulate(value):
     proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
     stdout_value, stderr_value = proc.communicate(value)
     return stdout_value
+
+def modulate(value):
+    if not value:
+        return ""
+    args = "modulator"
+    proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    stdout_value, stderr_value = proc.communicate(value)
+    return stdout_value
