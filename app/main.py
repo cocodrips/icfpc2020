4a57ddf80031d243f6a44b8d5ff07e0282aeecb8
YujiOshima <yuji.oshima0x3fd@gmail.com>
2020-07-19 12:12:19 +0900

    test try
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,20 +1,55 @@
 import requests
 import sys
+import urllib
+import subprocess
 
+def send_query(url, bit, is_real):
+    #if is_real:
+    #    url = 'https://icfpc2020-api.testkontur.ru/aliens/send'
+    #else:
+    #    url = 'https://dummy-server-prtffs3u5a-uc.a.run.app/aliens/send'
+    data = bit
+    #params = {
+    #'apiKey': "95052afa4bf54914a26622eea251b536",
+    #}
+    headers = {
+        'accept': '*/*',
+        'Content-Type': 'text/plain',
+    }
+    req = urllib.request.Request(url, data, headers)
+    with urllib.request.urlopen(req) as res:
+        body = res.read().decode('utf-8')
+        code = res.getcode()
+        #info = res.info()
+    return body.strip(), code
+
+def modulate(text):
+    url = "https://portal-prtffs3u5a-uw.a.run.app/modulate"
+    params = {
+    'value': text,
+    }
+
+    req = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
+    with urllib.request.urlopen(req) as res:
+        body = res.read().decode('utf-8').strip()
+    return body
 
 def main():
     server_url = sys.argv[1]
     player_key = sys.argv[2]
+    print("$free -mh")
+    p = subprocess.Popen("free -mh", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout_data, stderr_data = p.communicate()
+    print("{}, {}".format(stdout_data, stderr_data))
+    print("$cat /proc/cpuinfo")
+    p = subprocess.Popen("cat /proc/cpuinfo", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout_data, stderr_data = p.communicate()
+    print("{}, {}".format(stdout_data, stderr_data))
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
-
-    res = requests.post(server_url, data=player_key)
-    if res.status_code != 200:
-        print('Unexpected server response:')
-        print('HTTP code:', res.status_code)
-        print('Response body:', res.text)
-        exit(2)
-    print('Server response:', res.text)
-
+    mod_join = modulate("2 "+player_key)[:-2]+"110000"
+    print("Join query{}".format(mod_join))
+    res, code = send_query(server_url, mod_join, False)
+    print('Server response: {} code: {}'.format( res, code))
 
 if __name__ == '__main__':
     main()
