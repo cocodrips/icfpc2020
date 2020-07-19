cc041ec6f2fcf80334c3f361e25319bb9305fe99
YujiOshima <yuji.oshima0x3fd@gmail.com>
2020-07-19 15:01:07 +0900

    add start and command
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>

--- app/main.py
+++ app/main.py
@@ -32,17 +32,30 @@ def modulate(text):
 def main():
     server_url = sys.argv[1]
     player_key = sys.argv[2]
-    print("$free -mh")
-    p = subprocess.Popen("free -mh", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout_data, stderr_data = p.communicate()
-    print("{}".format(stdout_data.decode('utf-8')))
-    print("$cat /proc/cpuinfo")
-    p = subprocess.Popen("cat /proc/cpuinfo", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout_data, stderr_data = p.communicate()
-    print("{}".format(stdout_data.decode('utf-8')))
+
+    #Environment Investigation
+    #print("$free -mh")
+    #p = subprocess.Popen("free -mh", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #stdout_data, stderr_data = p.communicate()
+    #print("{}".format(stdout_data.decode('utf-8')))
+    #print("$cat /proc/cpuinfo")
+    #p = subprocess.Popen("cat /proc/cpuinfo", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #stdout_data, stderr_data = p.communicate()
+    #print("{}".format(stdout_data.decode('utf-8')))
+
     print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
     mod_join = modulate("2 "+player_key)[:-2]+"110000"
     print("Join query: {}".format(mod_join))
+    res, code = send_query(server_url, mod_join, False)
+    print('Server response: {} code: {}'.format( res, code))
+    
+    mod_start = modulate("3 "+player_key+" 0 0 0 0")
+    print("Start query: {}".format(mod_join))
+    res, code = send_query(server_url, mod_join, False)
+    print('Server response: {} code: {}'.format( res, code))
+
+    mod_join = modulate("4 "+player_key)[:-2]+"110000"
+    print("Command query: {}".format(mod_join))
     res, code = send_query(server_url, mod_join, False)
     print('Server response: {} code: {}'.format( res, code))
 
