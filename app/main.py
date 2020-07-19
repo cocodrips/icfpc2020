4c7dea8d01c8ad4450416606b96662a45ff108b0
YujiOshima <yuji.oshima0x3fd@gmail.com>
2020-07-19 21:44:22 +0900

    (nil,nil) query
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>

--- app/main.py
+++ app/main.py
@@ -75,13 +75,7 @@ def main():
     log={}
     ev = EventLogger(server_url=server_url, player_key=player_key)
     
-    query="1101000"
-    mod_query = "1101000"
-    res, code = send_query(server_url, mod_query, False)
-    ev.event_logging("get time?", query, mod_query, res, code)
-    
-
-    query="[2,"+player_key+",nil]"
+    query="(2,("+player_key+",(nil,nil)))"
     mod_query = modulate(query)
     res, code = send_query(server_url, mod_query, False)
     ev.event_logging("join", query, mod_query, res, code)
