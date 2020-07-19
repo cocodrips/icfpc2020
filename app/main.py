f303805413c3594e8c304729a532ab6caf2bd89d
YujiOshima <yuji.oshima0x3fd@gmail.com>
2020-07-20 01:49:12 +0900

    check start point
    Signed-off-by: YujiOshima <yuji.oshima0x3fd@gmail.com>

--- app/main.py
+++ app/main.py
@@ -87,8 +87,13 @@ def main():
     res, code = send_query(server_url, mod_query, False)
     demod_res = demodulate(res)
     ev.event_logging("join", query, mod_query, res, code, demod_res)
+
+    if "((446,(0,(0,(1,nil)))),nil)))))" in demod_res:
+        cood="[446,0,0,1)"
+    else:
+        cood="[0,0,0,0]"
     
-    query="[3,"+player_key+",[446,0,0,1]]"
+    query="[3,"+player_key+","+cood+"]"
     mod_query = modulate(query)
     res, code = send_query(server_url, mod_query, False)
     demod_res = demodulate(res)
