31a36a3b85145b750ce4c7a28a0399f69542e1b4
cocodrips <cocodrips@gmail.com>
2020-07-20 08:54:57 +0900

    Use python demodulator

--- portal/api/modem.py
+++ portal/api/modem.py
@@ -1,13 +1,11 @@
-import subprocess
 from api.thirdparty.modulate import modulate_from_string
+from api.thirdparty.demodulate import demodulate as  demodulate_from_string
+
 
 def demodulate(value):
     if not value:
         return ""
-    args = "modem dem".split()
-    proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-    stdout_value, stderr_value = proc.communicate(value)
-    return stdout_value
+    return demodulate_from_string(value)
 
 def modulate(value):
     if not value:
