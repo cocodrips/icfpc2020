b47da283c853e7e08b4fcda725f492a8dd18e008
Yusuke Izumi <yuizumi@y5i.org>
2020-07-19 20:54:31 +0900

    bugfix: don't reduce too much in IsNil.

--- yuizumi/eval/IsNil.java
+++ yuizumi/eval/IsNil.java
@@ -7,7 +7,7 @@ public class IsNil extends Func_1 {
     private IsNil() {}
 
     @Override public Expr invoke(Expr x0) {
-        return (x0.reduceToData() == Nil.EXPR) ? T.EXPR : F.EXPR;
+        return (x0.reduceToFunc() == Nil.EXPR) ? T.EXPR : F.EXPR;
     }
 
     @Override public String toString() { return "IsNil"; }
