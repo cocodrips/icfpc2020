f17b31a8a477f116c777812a2de7403b2f141a7b
cocodrips <cocodrips@gmail.com>
2020-07-20 21:14:43 +0900

    parse response

--- app/main.py
+++ app/main.py
@@ -1,14 +1,62 @@
 import requests
 import sys
+from dataclasses import dataclass
 
 import demodulate
 import modulate
-
 
 _API_KEY = '95052afa4bf54914a26622eea251b536'
 _JOIN = 2
 _START = 3
 _COMMANDS = 4
+
+
+@dataclass
+class Command:
+    commands: []
+
+
+@dataclass
+class Ship:
+    role: int
+    ship_id: int
+    position: (int, int)
+    velocity: (int, int)
+    x4: [int]
+    x5: int
+    x6: int
+    x7: int
+
+
+@dataclass
+class ShipAndCommand:
+    ship: Ship
+    command: Command
+
+
+@dataclass
+class GameInfo:
+    space_info: [int]
+
+
+@dataclass
+class GameState:
+    game_info: GameInfo
+    ship_and_commands: [ShipAndCommand]
+    size: int = 500
+
+
+def parse(data):
+    res, stage, static_game_info, game_state = data
+    x0, role, x2, x3, x4 = static_game_info
+    ship_and_command_obj: [ShipAndCommand] = []
+
+    if game_state:
+        game_tick, x1, ship_and_commands = game_state
+        for ship, commands in ship_and_commands:
+            ship_and_command_obj.append(ShipAndCommand(Ship(*ship), Command(commands)))
+
+    return GameState(GameInfo(x3), ship_and_command_obj)
 
 
 class Client(object):
@@ -38,9 +86,11 @@ def main(argv):
                     player_key)
 
     client.send(_JOIN, [])
-    client.send(_START, [200, 1, 0, 100])
+    res = client.send(_START, [200, 1, 0, 100])
+
     while True:
-        client.send(_COMMANDS, [[3,[0,0,0,1]]])
+        print(parse(res))
+        client.send(_COMMANDS, [])
 
 
 if __name__ == '__main__':
