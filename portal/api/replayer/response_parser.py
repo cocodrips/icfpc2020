7bf24151f86ba5099c0fdb05889bbfdd5c5630d8
cocodrips <cocodrips@gmail.com>
2020-07-20 11:13:33 +0900

    class dumper

--- portal/api/replayer/response_parser.py
+++ portal/api/replayer/response_parser.py
@@ -17,4 +17,5 @@ def parse(raw_data):
 
 if __name__ == '__main__':
     raw_data = '[1,1,[256,0,[512,1,64],[16,128],[97,47,11,1]],[0,[16,128],[[[1,0,(-29,48),(0,0),[97,47,11,1],0,64,1],nil],[[0,1,(29,-48),(0,0),[64,64,10,1],0,64,1],nil]]]]'
-    print(parse(raw_data))
+    game_state = parse(raw_data)
+    print(game_state)
